18879d47521ea03b2b8394e4a8f5eb15fdddc453
Nilambar Sharma <nilambar@outlook.com>
2020-06-08 18:18:24 +0545

    Update class

--- src/NepaliDate.php
+++ src/NepaliDate.php
@@ -17,5 +17,30 @@ namespace ErNilambar\NepaliDate;
  */
 class NepaliDate {
 
+	protected $calendar;
+
+	public function __construct() {
+		$this->calendar = new NepaliCalendar();
+	}
+
+	public function ad_to_bs( $y, $m, $d ) {
+		$output = array();
+
+		$new_date = $this->calendar->eng_to_nep( $y, $m, $d );
+
+		$output = $new_date;
+
+		return $output;
+	}
+
+	public function bs_to_ad( $y, $m, $d ) {
+		$output = array();
+
+		$new_date = $this->calendar->nep_to_eng( $y, $m, $d );
+
+		$output = $new_date;
+
+		return $output;
+	}
 
 }
