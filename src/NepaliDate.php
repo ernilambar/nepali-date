36165c85d88bdb942e44803e27f127d042da1428
Nilambar Sharma <nilambar@outlook.com>
2020-06-08 17:22:33 +0545

    Add class

---
+++ src/NepaliDate.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Nepali Date class
+ *
+ * @author    Nilambar Sharma <nilambar@outlook.com>
+ * @copyright 2020 Nilambar Sharma
+ *
+ * @package NepaliDate
+ */
+
+namespace ErNilambar\NepaliDate;
+
+/**
+ * NepaliDate class.
+ *
+ * @since 1.0.0
+ */
+class NepaliDate {
+
+
+}
